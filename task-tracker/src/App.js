584eaa9d30870025c2ec385caaa1a7ed5a7eb08f
mrsameer <sameershaik2001@outlook.com>
2021-02-27 13:25:03 +0530

    Added header.js as a component and index.css

---
+++ task-tracker/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/header";
+
+function App() {
+    return (
+        <div className="App">
+            <Header/>
+        </div>
+    );
+}
+
+// class based component
+// class App extends React.Component {
+//     render() {
+//         return <Header/>
+//     }
+// }
+export default App;
