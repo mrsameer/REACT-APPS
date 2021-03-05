b59b9ac6d992a0bb7c9c1e42a3db9c22f890798c
mrsameer <sameershaik2001@outlook.com>
2021-03-05 09:09:43 +0530

    Added Basiccomp2 view

---
+++ demo3/src/components/basiccomp2.js
@@ -0,0 +1,9 @@
+import React from "react"
+function Basiccomp2(params) {
+    // pure javascript code to display two headings 
+    return (
+        React.createElement('div', null, React.createElement('h1', null, 'This is h1'), React.createElement('h2', null, 'This is h2'))
+    )
+}
+
+export default Basiccomp2; 
