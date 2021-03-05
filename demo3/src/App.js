1f6578dc59262b52b0b1685500326c4f2918f451
mrsameer <sameershaik2001@outlook.com>
2021-03-05 21:10:23 +0530

    started hello world

--- demo3/src/App.js
+++ demo3/src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import Basiccomp from './components/basiccomp';
+import Basiccomp2 from './components/basiccomp2';
+import Basiccomp3 from './components/basiccomp3';
+import Basiccomp4 from './components/basiccomp4';
+import Basicprop from './components/basicpropexample';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        <Basiccomp/>
+       <Basiccomp2/>
+       <Basiccomp3/>
+       <Basiccomp4/>
+       <Basicprop name = "sameer"/>
       </header>
+
     </div>
   );
 }
