33f852d7d247b6f43c8633db086b7d67beae4d80
mrsameer <sameershaik2001@outlook.com>
2021-03-05 12:25:14 +0530

    Added span tag in demo3

---
+++ demo3/src/components/basiccomp3.js
@@ -0,0 +1,9 @@
+import React from "react"; 
+
+function Basiccomp3(params) {
+    return(
+       React.createElement('div', null, React.createElement('h1', null, React.createElement('span', null, 'This is span tag')))
+    )
+}
+
+export default Basiccomp3; 
